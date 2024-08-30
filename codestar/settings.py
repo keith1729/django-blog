03db253c56e1a3fa7e00e943a3b3d25461efa834
Keith O'Donoghue <amky2syl@students.codeinstitute.net>
2024-08-30 14:57:41 +0000

    Add filter published to views

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-keith1729-djangoblog-vg4jeqz01ub.ws-eu115.gitpod.io', '.herokuapp.com']
 
